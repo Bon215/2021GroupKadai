0cb89a2c83df0f6a0318fba9b84c6fc12a2210fb
Jang Yoonsung <upthe15752@gmail.com>
2021-11-30 16:14:35 +0900

    fix

--- scripts/server/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Running in NODE_ENV=$NODE_ENV COMMAND=$COMMAND"
-
-echo "Running npm install ..."
-npm install --prefer-offline --no-audit
-
-echo "Running npm run $COMMAND ..."
-yarn run $COMMAND
